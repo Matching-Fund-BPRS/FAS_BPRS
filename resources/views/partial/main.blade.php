fa34a4d81a8bfc4f31cc3967fe4c1546187b50db
fiikriaziz8 <fikri.maulana.aziz-2020@fst.unair.ac.id>
2023-10-06 08:37:04 +0700

    ganti title sama icon di head doang

--- resources/views/partial/main.blade.php
+++ resources/views/partial/main.blade.php
@@ -3,7 +3,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>BPRS Baktimakmur Indah</title>
+    <link rel="icon" href="{{ asset('img/logo.png') }}">
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css" rel="stylesheet" />
