faa7f516bb21932eca48d7d4fdb91224c0b47e42
thestrange300 <tegarsangkeyboardist@gmail.com>
2023-10-08 21:37:53 +0700

    redesigned login+register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,13 +1,51 @@
-<form method="post" action="{{ Route('login') }}">
-    @csrf
-    <input name="email" type="email" placeholder="Email">
-    <input name="password" type="password" placeholder="Password">
-    <button type="submit"> login </button>
-</form>
-<a href="/register"> ke register </a>
 
-@if(session('message'))
-    <script> 
-        alert('Daftar berhasil, silakan login!')
-    </script>
-@endif
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
+</head>
+<body>
+
+    <div class=" w-full h-full flex justify-center items-center drop-shadow-md">
+
+    <form method="post" action="{{ Route('login') }}">
+        @csrf
+        <div class="flex justify-center py-4">
+            <img src="{{ asset('img/logo.png') }}" alt="" class="w-auto h-24"> 
+        </div>
+        
+        <div class="align-middle space-y-4">
+            
+            <div>
+                <label for="sifat_plafond" class="block mb-2 text-xs font-medium text-gray-900">Email</label>
+                <input name="email" type="email" placeholder="Email" class="max-w-md shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5" required>
+            </div>
+    
+            <div>
+                <label for="sifat_plafond" class="block mb-2 text-xs font-medium text-gray-900">Password</label>
+                <input name="password" type="password" placeholder="Password" class="max-w-md shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5" required>
+            </div>
+            <div class=" items-center flex justify-center">
+                <button type="submit" class="text-white bg-gradient-to-b from-green-400 to-green-600 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Login</button>
+            </div>
+            
+        </div>
+        
+    </form>
+    
+    
+    @if(session('message'))
+        <script> 
+            alert('Daftar berhasil, silakan login!')
+        </script>
+    @endif
+
+</div>
+</body>
+</html>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,8 +1,54 @@
-<form method="post" action="{{ Route('register') }}">
+{{-- <form method="post" action="{{ Route('register') }}">
     @csrf
     <input name="name" type="text" placeholder="Nama"> 
     <input name="email" type="email" placeholder="Email">
     <input name="password" type="password" placeholder="Password">
     <button type="submit"> Register </button>
-</form>
-<a href="/login"> ke login </a>
+</form> --}}
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
+    <title>Register</title>
+</head>
+<body>
+    <div class="w-full h-full flex justify-center items-center drop-shadow-md">
+
+        <form method="post" action="{{ Route('register') }}" class=" mb-0">
+            @csrf
+            <div class="">
+                <div class="align-middle space-y-4">
+                
+                    <div>
+                        <label for="sifat_plafond" class="block mb-2 text-xs font-medium text-gray-900">Nama</label>
+                        <input name="name" type="text" placeholder="Nama" class="max-w-md shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5" required>
+                    </div>
+            
+                    <div>
+                        <label for="sifat_plafond" class="block mb-2 text-xs font-medium text-gray-900">Email</label>
+                        <input name="email" type="email" placeholder="Email" class="max-w-md shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5" required>
+                    </div>
+        
+                    <div>
+                        <label for="sifat_plafond" class="block mb-2 text-xs font-medium text-gray-900">Password</label>
+                        <input name="password" type="password" placeholder="Password" class="max-w-md shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5" required>
+                    </div>
+        
+                    <div class=" items-center flex justify-center">
+                        <button type="submit" class="text-white bg-gradient-to-b from-green-400 to-green-600 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Register</button>
+                    </div>
+                    
+                </div>
+            </div>
+        </form>
+
+    </div>
+</body>
+</html>
