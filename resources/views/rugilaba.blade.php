a2095949cb5975686d8e657fddb60b6de4d43622
NabilMustofaa <nabilmustofa6@gmail.com>
2023-10-25 12:39:56 +0700

    adding form on labarugi

--- resources/views/rugilaba.blade.php
+++ resources/views/rugilaba.blade.php
@@ -4,9 +4,10 @@
 
 <div class="flex flex-col mt-6">
     <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div class="inline-block min-w-full max-w-screen-xl py-2 align-middle md:px-6 lg:px-8">
-            <div class="overflow-hidden border border-gray-200 md:rounded-lg">
-                <form method="post" action="" id="laba_rugi_form">
+        <div class="inline-block min-w-full max-w-screen-xl py-2 align-middle md:px-6 lg:px-8 ">
+            <form method="post" action="" id="laba_rugi_form" class="flex flex-col">
+            <div class="overflow-hidden border border-gray-200 md:rounded-lg ">
+
                 <table class=" divide-y divide-gray-20 w-full table-auto overflow-auto whitespace-normal">
                     <thead class="bg-gray-50">
                         <tr>
@@ -40,8 +41,8 @@
                             <td class="px-12 py-4 font-medium whitespace-nowrap">
                                     <p class="text-sm font-normal text-center text-gray-600"></p>
                             </td>
-                            <td class="px-4 py-1 whitespace-nowrap">
-                                    <p class="text-sm font-semibold text-center text-gray-600">06/08/2015</p>
+                            <td class="px-4 py-1 whitespace-nowrap flex">
+                                    <input type="date" name="periode" class="text-sm font-normal text-center text-gray-600 border-none p-0 mx-auto">
                             </td>
                             <td class="px-4 py-1 whitespace-nowrap">
                                     <p class="text-sm font-semibold text-center text-gray-600">06/08/2016</p>
@@ -251,8 +252,11 @@
                 </table>
 
 
-                </form>
+
+
             </div>
+            <button type="submit" class="text-white bg-gradient-to-b from-green-400 to-green-600 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 justify-items-end ml-auto">Simpan</button>
+        </form>
         </div>
     </div>
 </div>
