c7635df266caf74c3393cd1385d8499c512404f7
NabilMustofaa <nabilmustofa6@gmail.com>
2023-11-15 18:49:05 +0700

    delete dd

--- app/Http/Controllers/InfoKeuanganController.php
+++ app/Http/Controllers/InfoKeuanganController.php
@@ -13,7 +13,7 @@ use App\Models\TRugilaba;
 class InfoKeuanganController extends Controller
 {
     public function addInfoKeuangan(Request $request){
-        dd($request->all());
+        
 
         $omset = str_replace('.', '', $request->omset);
         $biaya_gaji = str_replace('.', '', $request->biaya_gaji);
@@ -84,6 +84,7 @@ class InfoKeuanganController extends Controller
                 'BIAYA_LAIN' => $biaya_angsuran_lain
             ]);
         }
+
 
         $hpp = str_replace('.', '', $request->total_biaya);
         $biaya_hidup = str_replace('.', '', $request->total_biaya_rt);
